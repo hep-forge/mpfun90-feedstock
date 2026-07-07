8dc352c3e5e9156045374a524bb64247b83cc1ac
Marco Meyer-Conde <marco@tcu.ac.jp>
2026-07-07 13:22:04 +0900

    Fill in real source, license, and build steps for MPFUN90 2024-12-06

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,8 @@
 #! /usr/bin/bash
 set -e
 
-mkdir -p build
-cd build
-
-cmake .. \
-  -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-  -DCMAKE_BUILD_TYPE=Release
-
-NPROC=$(nproc 2>/dev/null || sysctl -n hw.ncpu)
-make -j"$NPROC"
-make install
+make dynamic FC="${FC}" FFLAGS="${FFLAGS}"
+mv "libmpfun90${SHLIB_EXT}" "${PREFIX}/lib/"
+mkdir -p "${PREFIX}/include/mpfun90"
+mv *.mod "${PREFIX}/include/mpfun90/"
+make clean
